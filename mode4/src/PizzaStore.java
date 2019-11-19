20fab51c7baac8c44e945e5b0a30891b422c8a8a
Daisy <8893518+liyandev@users.noreply.github.com>
2019-11-19 14:22:35 +0800

    Update PizzaStore.java

--- mode4/src/PizzaStore.java
+++ mode4/src/PizzaStore.java
@@ -7,10 +7,15 @@
 *
 * */
 public abstract class PizzaStore {
-
+    SimplePizzaFactory factory;
+    
+    public PizzaStore(SimplePizzaFactory factory){
+        this.factory = factory;
+    }
+    
     public Pizza orderPizza(String type){
         Pizza pizza;
-        pizza = createPizza(type);
+        pizza = factory.createPizza(type);
 
         pizza.prepare();
         pizza.bake();
@@ -18,6 +23,4 @@ public abstract class PizzaStore {
         pizza.box();
         return pizza;
     }
-
-    abstract Pizza createPizza(String type);
 }
